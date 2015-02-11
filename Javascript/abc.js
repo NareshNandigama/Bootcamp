5d12b206ae54525f08aaca30cf9e453e02d7a30b
gs-nnandigama <gs-nnandigama@gainsight.com>
2015-02-11 17:17:50 +0530

    all programs commit.

---
+++ Javascript/abc.js
@@ -0,0 +1,453 @@
+
+var data=[{
+		"ASV" : 12345.72,
+		"Users" : 5,
+		"Id" : "a099000001DIDq2AAH",
+		"Name" : "sadasd",
+		"TaskCount" : 0,
+		"OpenTaskCount" : 0,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlaggedc" : false,
+		"SnoozedUntilc" : "",
+		"Accountc" : "0019000000vbRhbAAE",
+		"AccountName" : "Abbett Electric Corp",
+		"AccountCustomerInfo" : "a0B9000000JAi2DEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#c53536",
+		"AccountCustomerInfoCurScoreIdName" : "20",
+		"PriorityName" : "Medium",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Risk",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421712000000,
+		"CreatedDate" : 1422403200000,
+		"Age" : 5,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 100,
+		"Id" : "a099000001CjYVlAAN",
+		"Name" : "New CTA",
+		"TaskCount" : 1,
+		"OpenTaskCount" : 1,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlaggedc" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "Medium",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Opportunity",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421971200000,
+		"CreatedDate" : 1421798400000,
+		"Age" : 2,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 123,
+		"Id" : "a099000001C7UGoAAN",
+		"Name" : "TEST CTA",
+		"TaskCount" : 1,
+		"OpenTaskCount" : 1,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "Low",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Event",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421971200000,
+		"CreatedDate" : 1421798400000,
+		"Age" : 22,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 50,
+		"Id" : "a099000001C7UJEAA3",
+		"Name" : "asdad",
+		"TaskCount" : 1,
+		"OpenTaskCount" : 1,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "Low",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Risk",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421971200000,
+		"CreatedDate" : 1421798400000,
+		"Age" : 12,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 25,
+		"Id" : "a099000001C3Tm4AAF",
+		"Name" : "Apply playbook",
+		"TaskCount" : 4,
+		"OpenTaskCount" : 4,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "Low",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Event",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1423526400000,
+		"CreatedDate" : 1421452800000,
+		"Age" : 9,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 33,
+		"Id" : "a099000001C3TmjAAF",
+		"Name" : "CTA5",
+		"TaskCount" : 5,
+		"OpenTaskCount" : 5,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "High",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "In-Progress",
+		"StageId" : "a0P9000000GysvgEAB",
+		"TypeName" : "Risk",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1425254400000,
+		"CreatedDate" : 1421452800000,
+		"Age" : 16,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1099800,
+		"Users" : 20,
+		"Id" : "a099000001B1Am5AAF",
+		"Name" : "CTA2",
+		"TaskCount" : 4,
+		"OpenTaskCount" : 0,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "High",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "Closed Won",
+		"StageId" : "a0P9000000GysvhEAB",
+		"TypeName" : "Risk",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421798400000,
+		"CreatedDate" : 1421193600000,
+		"Age" : 6,
+		"ClosedDate" : 1421712000000
+	}, {
+		"ASV" : 1099800,
+		"Users" : 15,
+		"Id" : "a099000001B1AvHAAV",
+		"Name" : "CTA1",
+		"TaskCount" : 2,
+		"OpenTaskCount" : 0,
+		"AssigneeSmallPhotoUrl" : "https://c.ap1.content.force.com/profilephoto/729900000002Lev/T",
+		"AssigneeName" : "Akhila Kotagiri",
+		"AssigneeId" : "00590000002c1vuAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "0019000000vbRhrAAE",
+		"AccountName" : "Abacus Programming Corporation with larger account name",
+		"AccountCustomerInfo" : "a0B9000000JAi2TEAT",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "Score",
+		"AccountCustomerInfoCurScoreIdColor" : "#eba638",
+		"AccountCustomerInfoCurScoreIdName" : "75",
+		"PriorityName" : "Low",
+		"PriorityId" : "a0P9000000HvTEaEAN",
+		"StageName" : "Closed Won",
+		"StageId" : "a0P9000000GysvhEAB",
+		"TypeName" : "Event",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a089000000X5waqAAB",
+		"DueDate" : 1421366400000,
+		"CreatedDate" : 1421193600000,
+		"Age" : 1,
+		"ClosedDate" : 1421280000000
+	},
+	{
+		"ASV" : 1067800,
+		"Users" : 10,
+		"NextRenewalDate" : "",
+		"Id" : "a0Bo0000001jaCYEAY",
+		"Name" : "UserLookup",
+		"TaskCount" : 1,
+		"OpenTaskCount" : 1,
+		"AssigneeSmallPhotoUrl" : "https://c.na17.content.force.com/profilephoto/005/T",
+		"AssigneeName" : "Ranjith Kumar",
+		"AssigneeId" : "005o0000000mVrYAAU",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "001o000000JBiMIAA1",
+		"AccountName" : "Abacus Programming Corporation",
+		"AccountCustomerInfo" : "a0Do0000002VhxaEAC",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "",
+		"AccountCustomerInfoCurScoreIdColor" : "",
+		"AccountCustomerInfoCurScoreIdName" : "",
+		"PriorityName" : "High",
+		"PriorityId" : "a0To0000001CTVIEA4",
+		"StageName" : "Open",
+		"StageId" : "a0To0000001CTUAEA4",
+		"TypeName" : "Risk",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a0Ao0000005uo2qEAA",
+		"DueDate" : 1419552000000,
+		"CreatedDate" : 1418947200000,
+		"Age" : 10,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 1033800,
+		"Users" : 30,
+		"NextRenewalDate" : "",
+		"Id" : "a0Bo0000001jaSMEAY",
+		"Name" : "DefaultOwner_userlookup",
+		"TaskCount" : 0,
+		"OpenTaskCount" : 0,
+		"AssigneeSmallPhotoUrl" : "https://c.na17.content.force.com/profilephoto/729o0000000dVtn/T",
+		"AssigneeName" : "hari123 V",
+		"AssigneeId" : "005o0000000mBy6AAE",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "001o000000JBiLsAAL",
+		"AccountName" : "AAR Corp Hardware Abscoa Division",
+		"AccountCustomerInfo" : "a0Do0000002VhxAEAS",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "",
+		"AccountCustomerInfoCurScoreIdColor" : "",
+		"AccountCustomerInfoCurScoreIdName" : "",
+		"PriorityName" : "Medium",
+		"PriorityId" : "a0To0000001CTV6EAO",
+		"StageName" : "Open",
+		"StageId" : "a0To0000001CTUAEA4",
+		"TypeName" : "Opportunity",
+		"TypeColor" : "#42b899",
+		"TypeId" : "a0Ao0000005uo2rEAA",
+		"DueDate" : 1419552000000,
+		"CreatedDate" : 1418947200000,
+		"Age" : 25,
+		"ClosedDate" : ""
+	}, {
+		"ASV" : 999800,
+		"Users" : 25,
+		"NextRenewalDate" : "",
+		"Id" : "a0Bo0000001jaQpEAI",
+		"Name" : "Standard_UserLookup_NotDefaultOwner",
+		"TaskCount" : 4,
+		"OpenTaskCount" : 4,
+		"AssigneeSmallPhotoUrl" : "https://c.na17.content.force.com/profilephoto/005/T",
+		"AssigneeName" : "Ashok",
+		"AssigneeId" : "005o0000000me1uAAA",
+		"IsFlagged" : false,
+		"SnoozedUntil" : "",
+		"Account" : "001o000000JBiNPAA1",
+		"AccountName" : "Abberley Kooiman",
+		"AccountCustomerInfo" : "a0Do0000002VhyhEAC",
+		"AccountCustomerInfoCurScoreIdSchemeIdType" : "",
+		"AccountCustomerInfoCurScoreIdColor" : "",
+		"AccountCustomerInfoCurScoreIdName" : "",
+		"PriorityName" : "High",
+		"PriorityId" : "a0To0000001CTVIEA4",
+		"StageName" : "Open",
+		"StageId" : "a0To0000001CTUAEA4",
+		"TypeName" : "Opportunity",
+		"TypeColor" : "#f45655",
+		"TypeId" : "a0Ao0000005uo2qEAA",
+		"DueDate" : 1420156800000,
+		"CreatedDate" : 1418947200000,
+		"Age" : 30,
+		"ClosedDate" : ""
+	}
+];
+
+
+ function myFunction() {
+ 	 var arr=[];
+    for (var i = 0; i <data.length; i++) 
+    {
+    	
+ 	arr.push(data[i].PriorityName);
+ 	//console.log(arr[i]+"ssssssssssssssssssss");
+   	
+   		for (var j = 0; j <arr.length; j++) 
+   		{	
+   			var count=0;
+   			for (var i = 0; i < arr.length; i++) {
+   				//console.log(arr[i]);
+   			
+   			if (arr[j]==arr[i]) {
+   				count++;
+   				//console.log(count);
+   				if (count>1) {
+           		continue;
+           		//obj[arr[i]]=count;
+
+   			}};
+   			
+    	}};//document.write(JSON.stringify(obj));
+    if (count==1) {
+    var option = document.createElement("option");
+    option.text = data[i].PriorityName;
+   
+   	var x=document.getElementById("prioritySelect");
+    x.appendChild(option);
+    }
+    };
+    //OWner=AssigneeName,,,,,,,,,,,,,,,,,,,
+    var arr1=[];
+    for (var i = 0; i <data.length; i++) 
+    {
+    	
+ 	arr1.push(data[i].AssigneeName);
+ 	//console.log(arr1[i]+"ssssssssssssssssssss");
+   	
+   		for (var j = 0; j <arr1.length; j++) 
+   		{	
+   			var count1=0;
+   			for (var i = 0; i < arr1.length; i++) {
+   				//console.log(arr[i]);
+   			
+   			if (arr1[j]==arr1[i]) {
+   				count1++;
+   				//console.log(count);
+   				if (count1>1) {
+           		continue;
+           		//obj[arr[i]]=count;
+
+   			}};
+   			
+    	}};//document.write(JSON.stringify(obj));
+    if (count1==1) {
+    var option1 = document.createElement("option");
+    option1.text = data[i].AssigneeName;
+   
+   	var x1=document.getElementById("ownerSelect");
+    x1.appendChild(option1);
+    }
+    };
+}
+myFunction();
+function changepriority(evt){
+table=document.getElementById("table1");
+var l=table.rows.length;
+console.log(l);
+for (var i = l-1; i >1 ; i--) {
+table.deleteRow(i);
+}
+	for (var i = 0; i <data.length; i++) 
+    {
+    if (evt.value ==data[i].PriorityName) {
+	    row=table.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        cell1.innerHTML =data[i].AssigneeName;
+        cell2.innerHTML = data[i].AccountName;
+        cell3.innerHTML = data[i].ASV;
+        cell4.innerHTML = data[i].Users;
+        cell5.innerHTML = data[i].TypeName;
+        cell6.innerHTML = data[i].Age;
+		}
+	}
+	var l3=table.rows.length;
+	var x3=document.getElementById("rowcount");
+    x3.innerHTML=l3-2;
+}
+/////////////////////////////////
+function changeowner(e){
+table=document.getElementById("table1");
+var l1=table.rows.length;
+console.log(l1);
+for (var i = l1-1; i >1 ; i--) {
+table.deleteRow(i);
+}
+  for (var i = 0; i <data.length; i++) 
+    { 	
+	if (e.value ==data[i].AssigneeName) {
+	    row=table.insertRow();
+        var c1 = row.insertCell(0);
+        var c2 = row.insertCell(1);
+        var c3 = row.insertCell(2);
+        var c4 = row.insertCell(3);
+        var c5 = row.insertCell(4);
+        var c6 = row.insertCell(5);
+        c1.innerHTML =data[i].AssigneeName;
+        c2.innerHTML = data[i].AccountName;
+        c3.innerHTML = data[i].ASV;
+        c4.innerHTML = data[i].Users;
+        c5.innerHTML = data[i].TypeName;
+        c6.innerHTML = data[i].Age;
+		}
+	}
+	var l2=table.rows.length;
+	var x2=document.getElementById("rowcount");
+    x2.innerHTML=l2-2;
+}
